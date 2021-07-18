8a582ea38aae009e98d3745adb6e61fb0dd1e265
kuro <toshras22@gmail.com>
2021-07-18 09:53:07 +0900

    gzip圧縮

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
-const mix = require('laravel-mix');
+const mix = require("laravel-mix");
+const CompressionPlugin = require("compression-webpack-plugin");
 
 /*
  |--------------------------------------------------------------------------
@@ -10,24 +11,20 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
-// ESLintに関する設定（この部分を丸ごと追記するイメージです）
-// if (!mix.inProduction()) { // 本番環境ではESLintは使用しない
-//     mix.webpackConfig({
-//         module: {
-//             rules: [
-//                 {
-//                     enforce: 'pre',
-//                     exclude: /node_modules/,
-//                     loader: 'eslint-loader',
-//                     test: /\.(js|vue)?$/,
-//                 },
-//     e        ],
-//         },
-//     })
-// }
-
-
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
-
-mix.browserSync('http://127.0.0.1:8000/');
+mix.js("resources/js/app.js", "public/js")
+    .sass("resources/sass/app.scss", "public/css")
+    .webpackConfig({
+        plugins: [
+            new CompressionPlugin({
+                filename: "[path].gz[query]",
+                algorithm: "gzip",
+                test: /\.js$|\.css$|\.html$|\.svg$/,
+                threshold: 10240,
+                minRatio: 0.8
+            })
+        ]
+    });
+if (mix.inProduction()) {
+    mix.version();
+}
+mix.browserSync("http://127.0.0.1:8000/");
