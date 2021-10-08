e21ebe024bf6f69a559c428d34bab9d0aad4b4b0
kuro <toshras22@gmail.com>
2021-10-08 19:46:50 +0900

    サイドバーにアイコン追加

--- resources/js/components/GuestHeaderComponent.vue
+++ resources/js/components/GuestHeaderComponent.vue
@@ -9,17 +9,17 @@
         <v-list class="mt-8 font-weight-bold">
           <v-list-item link href="/recipeList">
             <v-list-item-content>
-              <v-list-item-title>レシピ一覧</v-list-item-title>
+              <v-list-item-title><span class="icon_area"><v-icon class="mr-3">fas fa-list</v-icon></span>レシピ一覧</v-list-item-title>
             </v-list-item-content>
           </v-list-item>
           <v-list-item link href="/login">
             <v-list-item-content>
-              <v-list-item-title>ログイン</v-list-item-title>
+              <v-list-item-title><span class="icon_area"><v-icon class="mr-3" font-size="40">fas fa-sign-in-alt</v-icon></span>ログイン</v-list-item-title>
             </v-list-item-content>
           </v-list-item>
           <v-list-item link href="/register">
             <v-list-item-content>
-              <v-list-item-title>新規登録</v-list-item-title>
+              <v-list-item-title><span class="icon_area"><v-icon class="mr-3" width="30">fas fa-user-plus</v-icon></span>新規登録</v-list-item-title>
             </v-list-item-content>
           </v-list-item>
         </v-list>
