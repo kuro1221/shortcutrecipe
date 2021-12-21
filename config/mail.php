7b79a63902c72daf8527cc2aaabeb0f8a7685f50
kuro <toshras22@gmail.com>
2021-12-21 22:05:00 +0900

    gmailでメールを送信できるよう修正

--- config/mail.php
+++ config/mail.php
@@ -29,7 +29,7 @@ return [
     |
     */
 
-    'host' => env('MAIL_HOST', 'smtp.aol.com'),
+    'host' => env('MAIL_HOST', 'smtp.gmail.com'),
 
     /*
     |--------------------------------------------------------------------------
@@ -42,7 +42,7 @@ return [
     |
     */
 
-    'port' => env('MAIL_PORT', 465),
+    'port' => env('MAIL_PORT', 587),
 
     /*
     |--------------------------------------------------------------------------
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'shortcutrecipe@aol.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'shortcutrecipe12@gmail.com'),
         'name' => env('MAIL_FROM_NAME', '管理人'),
     ],
 
